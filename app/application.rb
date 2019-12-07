b61691aa75c833122854051a8bc32c177d907604
Adam Weissman <adam@comedytragedyepic.com>
2019-12-07 13:21:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,16 +7,19 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item =@@items.find {|i| i.name == item_name}
-        resp.write item.price
-      else 
-        resp.status = 400
+      item = @@items.find{|i| i.name == item_name}
+
+    if item.nil?
         resp.write "Item not found"
+        resp.status = 400
+    else
+        resp.write item.price
       end
     else
-      resp.status=404
       resp.write "Route not found"
+      resp.status = 404
     end
+    
     resp.finish
   end
 end
